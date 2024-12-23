17373e8de5781d100f2b184c83b7a434f031399a
Rene Barahona <rene.barahona@pnmac.com>
2024-12-23 15:11:31 -0800

    fix typo in gcc build configure; tweak tar dl

--- ec2/bootstrap.sh
+++ ec2/bootstrap.sh
@@ -18,10 +18,15 @@ dnf update -y
 dnf install -y $(cat ec2/dnf-requirements.txt | xargs)
 
 section "preparing gcc-prereqs"
-wget -O /tmp/gcc.tar.gz https://ftp.gnu.org/gnu/gcc/gcc-4.9.4/gcc-4.9.4.tar.gz
-mkdir /tmp/gcc && tar -C /tmp/gcc --strip-components 1 -xzvf /tmp/gcc.tar.gz
+if [[ ! -d /tmp/gcc ]]; then
+    wget -O /tmp/gcc.tar.gz https://ftp.gnu.org/gnu/gcc/gcc-4.9.4/gcc-4.9.4.tar.gz
+    mkdir /tmp/gcc && tar -C /tmp/gcc --strip-components 1 -xzvf /tmp/gcc.tar.gz
+fi
+
 cd /tmp/gcc && ./contrib/download_prerequisites
-$(find /usr/share -maxdepth 1 -type d -name 'automake*')/config.guess > /var/build_host
+if [[ ! -f /var/build_host ]]; then
+    $(find /usr/share -maxdepth 1 -type d -name 'automake*')/config.guess > /var/build_host
+fi
 
 info "build_host: $(< /var/build_host)"
 
@@ -42,4 +47,4 @@ cd -
 
 section "starting gcc build"
 export LD_PRELOAD=/usr/lib64/libstdc++.so.6
-./configure --disable-multilib --enable-languages=c,c++ --build=$(< /var/build_host4)s
+./configure --disable-multilib --enable-languages=c,c++ --build=$(< /var/build_host)
