873b202476b3ddffbc0683baa177f686787e1cbb
Rene Barahona <rene.barahona@pnmac.com>
2024-12-23 17:10:35 -0800

    apply patches

--- scripts/apply-patches.sh
+++ scripts/apply-patches.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 for file in $(find src/gcc/patches -type f -name '*.patch'); do
-    echo "${file%".patch"}"
+    source="$(realpath $file)"
+    target="/tmp/gcc/$(realpath --relative-to src/gcc/patches "${file%.patch}")"
+    echo -e "applying\n\t$source --> $target"
+    cd "$(dirname $target)"
+    patch < "$source"
+    cd -
 done
