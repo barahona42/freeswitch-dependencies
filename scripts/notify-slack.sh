89f255959e878d89afffa8241b137414e986c4d2
Rene Barahona <rene.barahona@pnmac.com>
2024-12-26 14:24:38 -0800

    updates

--- scripts/notify-slack.sh
+++ scripts/notify-slack.sh
@@ -8,5 +8,5 @@ test -s $WEBHOOK_URL
 TEXT="$1"
 TARGET_FILE="$2"
 
-data=$(jq -nrc --arg text "$TEXT" --arg logtail "$(printf "\`\`\`%s\`\`\`" "$(tail -n10 $TARGET_FILE)")" '{"blocks":[{"type":"divider"},{"type":"section","text":{"type":"mrkdwn","text":$text}},{"type":"divider"},{"type":"section","text":{"type":"mrkdwn","text":$logtail}}]}')
+data=$(jq -nrc --arg text "$TEXT" --arg logtail "$(printf "\`\`\`%s\`\`\`" "$(tail -n5 $TARGET_FILE)")" '{"blocks":[{"type":"divider"},{"type":"section","text":{"type":"mrkdwn","text":$text}},{"type":"divider"},{"type":"section","text":{"type":"mrkdwn","text":$logtail}}]}')
 curl -f -X POST -H 'Content-Type: application/json' --data "$data" "$WEBHOOK_URL"
