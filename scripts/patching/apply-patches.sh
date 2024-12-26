c8af8f3ee5b101198d2fa4254d94635f1c14f186
Rene Barahona <rene.barahona@pnmac.com>
2024-12-26 13:08:06 -0800

    fix pathes path

--- scripts/patching/apply-patches.sh
+++ scripts/patching/apply-patches.sh
@@ -1,9 +1,9 @@
-for file in $(cat /var/gcc-patches/target-files.txt); do
+for file in $(cat gcc-patches/target-files.txt); do
     # make a backup of the original
     test -f /var/gcc/$file || (echo "missing target file: /var/gcc/$file" && exit 1)
     cp -v /var/gcc/$file /var/gcc/$file.orig
     # find the patch file
-    test -f /var/gcc-patches/$file.patch || (echo "missing patch file: /var/gcc-patches/$file" && exit 1)
-    patch -fi /var/gcc-patches/$file.patch /var/gcc/$file
+    test -f gcc-patches/$file.patch || (echo "missing patch file: gcc-patches/$file" && exit 1)
+    patch -fi gcc-patches/$file.patch /var/gcc/$file
     diff -y --suppress-common-lines --suppress-blank-empty /var/gcc/$file /var/gcc/$file.orig
 done
