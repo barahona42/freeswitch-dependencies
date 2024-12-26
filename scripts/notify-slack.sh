fd087a9ff05823364003b0aa15e557f514032893
Rene Barahona <rene.barahona@pnmac.com>
2024-12-26 13:39:56 -0800

    tail file from arg

--- scripts/notify-slack.sh
+++ scripts/notify-slack.sh
@@ -6,6 +6,7 @@ source .local/.env
 test -s $WEBHOOK_URL
 
 TEXT="$1"
+TARGET_FILE="$2"
 
-data=$(jq -nrc --arg text "$TEXT" --arg logtail "$(printf "\`\`\`%s\`\`\`" "$(cat customizations.sh)")" '{"blocks":[{"type":"divider"},{"type":"section","text":{"type":"mrkdwn","text":$text}},{"type":"divider"},{"type":"section","text":{"type":"mrkdwn","text":$logtail}}]}')
+data=$(jq -nrc --arg text "$TEXT" --arg logtail "$(printf "\`\`\`%s\`\`\`" "$(tail -n10 $TARGET_FILE)")" '{"blocks":[{"type":"divider"},{"type":"section","text":{"type":"mrkdwn","text":$text}},{"type":"divider"},{"type":"section","text":{"type":"mrkdwn","text":$logtail}}]}')
 curl -f -X POST -H 'Content-Type: application/json' --data "$data" "$WEBHOOK_URL"
